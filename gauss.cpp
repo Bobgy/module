83f2d6a7cf4dc2f9d38c28a894bc3c31570e8f44
Bobgy <gongyuan94@gmail.com>
2014-09-15 01:15:42 +0800

    Update gauss.cpp

--- gauss.cpp
+++ gauss.cpp
@@ -1,144 +1,42 @@
 #include <cmath>
-#include <cstdio>
 #include <cstring>
 #include <algorithm>
-#include <iostream>
-#include <vector>
-#include <assert.h>
-#include <cstdlib>
-#include <ctime>
-
 using namespace std;
+//#define DEBUG
+typedef double flt;
+const flt eps=1e-9;
+const int MAXN=512, MAXM=512;
 
-const int SIZE=10;
-typedef vector<double> vd;
-typedef vector<int> vi;
-typedef vector<vd> vvd;
-vvd a;
-vd b;
-const int MAXN=50000;
-const int MAXM=5000;
-const int MAXK=3;
-const double eps=1e-10;
+flt e[MAXN][MAXM], x[MAXM];
+int pos[MAXM];
 
-
-vvd transpose(const vvd &M)
-{
-    vvd a(M);
-    for(int i=0;i<a.size();i++)
-        for(int j=i+1;j<a.size();j++)
-            swap(a[i][j],a[j][i]);
-    return a;
-}
-
-void print(const vvd &a)
-{  
-    cout<<"----------"<<endl;
-    for(int i=0;i<a.size();i++)
-        for(int j=0;j<a[i].size();j++)
-            printf("%.2lf%c",a[i][j],j==a[i].size()-1?'\n':' ');
-}
-
-void print(const vd &a)
-{ 
-    for(int i=0;i<a.size();i++)
-        printf("%.2lf%c",a[i],i==a.size()-1?'\n':' ');
-}
-
-vvd reduce(const vvd &M)  //good
-{
-    vvd a(M);
-    assert(a.size());
-    int n=a.size(),m=a[0].size();
-    for(int i=1;i<n;i++)assert(a[i].size()==m);
-	double tmp; int t;
-	int j=-1,i=0;
-	for (; i<n; i++){
-	    do{
-            j++;
-            if(j>=m)break;
-            t=i;
-            for (int k=i+1; k<n; k++)
-                if (fabs(a[k][j])>fabs(a[t][j])) t=k;
-        } while(fabs(a[t][j])<eps);
-        if(j>=m)break;
-		for (int k=j; k<m; k++) swap(a[i][k], a[t][k]);
-		for (int k=j+1; k<m; k++) a[i][k]/=a[i][j];
-		a[i][j]=1;
-        for(int k=0;k<n;k++)
-        {
-            if(k==i)continue;
-            for(int l=j+1;l<m;l++)
-                a[k][l]-=a[k][j]*a[i][l];
-            a[k][j]=0;
+//n*(m+1) matrix
+//-1 means no solution; otherwise return rank
+int solve(int n, int m){
+    int r=0;
+    //c for column, r for row
+    for(int c=0,i,j,k;c<=m && r<n;++c){
+        for(i=k=r;i<n;++i)if(fabs(e[i][c])>fabs(e[k][c]))k=i;
+        if(k!=r)
+            for(j=c;j<=m;++j)swap(e[k][j],e[r][j]);
+        if(fabs(e[r][c])<eps)continue;
+        pos[r]=c;
+        for(i=r+1;i<n;++i){
+            flt t=-e[i][c]/e[r][c];
+            for(int j=c;j<=m;++j)e[i][j]+=t*e[r][j];
         }
-	}
-	a.resize(i);
-	return a;
-}
-
-int gauss(const vvd &a, const vd &b, vd &res) //not recommended
-{
-    assert(a.size()==b.size());
-    res.clear();
-	vvd d(reduce(a));
-	if(d.size()<a.size()){ //multiple or none
-        int last=0;
-        vvd t;
-        int result=0;
-        for(int i=0;i<a.size();i++)
-        {
-            t.push_back(a[i]);
-            //print(t);
-            vvd tmp(reduce(t));
-            //print(tmp);
-            int rr=tmp.size();
-            if(rr==last)result+=(1<<i);
-            last=rr;
-        }
-        return result;
+        r++;
     }
-    vvd c(a);
-    for(int i=0;i<a.size();i++)c[i].push_back(b[i]);
-	vvd r(reduce(c));
-	assert(r.size()==a.size());
-    for(int i=0;i<r.size();i++)res.push_back(r[i].back());
-	return 0;
-}
-
-int n, m;
-
-main()
-{
-    srand(time(NULL));
-    int n=8;
-    b=vd(n,1);
-    a=vvd(n,vd(n,1));
-    for(int i=0;i<n;i++){
-        b[i]=rand()%10;
-        for(int j=0;j<n;j++)
-            a[i][j]=rand()%10;
+#ifdef DEBUG
+    REP(i,n)REP(j,m+1)printf("%4.2f%c",(double)e[i][j],j==m?'\n':' ');
+    puts("=========");
+#endif
+    if(r>0 && pos[r-1]==m)return -1; //no solution
+    for(int i=0;i<m;++i)x[i]=0;
+    for(int i=r-1;i>=0;--i){
+        int c=pos[i];
+        x[c]=e[i][m]/e[i][c];
+        for(int j=0;j<i;++j)e[j][m]-=e[j][c]*x[c];
     }
-    print(a);
-    vvd t1(reduce(a));
-    print(t1);
-    cout<<t1.size()<<endl;
-    vvd c(transpose(a));
-    print(c);
-    vvd d(reduce(c));
-    print(d);
-    vd res;
-    int flag=gauss(a,b,res);
-    cout<<flag<<endl;
-    if(flag){
-        int k=0;
-        while(flag)
-        {
-            if(flag%2)cout<<k<<' ';
-            k++;
-            flag/=2;
-        }
-        cout<<endl;
-    } else print(res);
+    return r;
 }
-
