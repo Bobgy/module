2bc8ea429048ad8029d3220b80202de4816afbc2
Bobgy <gongyuan94@gmail.com>
2015-10-02 19:55:31 +0800

    Updated head.cpp

--- head.cpp
+++ head.cpp
@@ -1,50 +1,22 @@
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <numeric>
-#include <set>
-#include <map>
-#include <queue>
-#include <iostream>
-#include <sstream>
-#include <cstdio>
-#include <cmath>
-#include <ctime>
-#include <cstring>
-#include <cctype>
-#include <utility>
-#include <cstdlib>
-#include <cassert>
-
-#define rep(i,n) for(int (i)=0;(i)<(int)(n);++(i))
-#define foreach(c,itr) for(__typeof((c).begin()) itr=(c).begin();itr!=(c).end();itr++)
-#define rer(i,l,u) for(int (i)=(int)(l);(i)<=(int)(u);++(i))
-#define reu(i,l,u) for(int (i)=(int)(l);(i)<(int)(u);++(i))
-#define all(o) (o).begin(), (o).end()
-#define pb(x) push_back(x)
-#define mp(x,y) make_pair((x),(y))
-#define mset(m,v) memset(m,v,sizeof(m))
-#define EPS 1e-9
-#define gi(a) scanf("%d",&a)
-#define gi2(a,b) scanf("%d%d",&a,&b)
-#define gi3(a,b,c) scanf("%d%d",&a,&b,&c)
-#define read(_type,name) _type name; cin>>name
-#define read2(_type,name1,name2) _type name1,name2; cin>>name1>>name2
-#define read3(_type,name1,name2,name3) _type name1,name2,name3; cin>>name1>>name2>>name3
-#define pi 3.141592653589793238
-#define INT_INF 0x7fffffff
-#define UI_INF 0xffffffff
-#define PI 3.141592653589793238462643
+#include <bits/stdc++.h>
 using namespace std;
-
-typedef vector<int> vi; typedef pair<int,int> pii; typedef vector<pair<int,int> > vpii;
-typedef long long ll; typedef pair<double,double> pdd; typedef unsigned int ui;
-
-#define maxn 0xffff
-
-main()
-{
-    //std::ios::sync_with_stdio(false);
-    int n,p;
+#define DEBUG
+#ifdef DEBUG
+    #define out(x) (cerr<<#x"="<<x<<endl,(x))
+    #define debug(args...) fprintf(stderr, args)
+#else
+    #define out(x) (x)
+    #define debug(...)
+#endif
+#define rep(i,n) for(int i=0;i<int(n);++i)
+#define rer(i,L,R) for(int i=int(L);i<=int(R);++i)
+#define all(o) (o).begin(),(o).end()
+#define pb push_back
+#define X first
+#define Y second
+typedef long long LL;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef vector<pair<int,int> > vpii;
+int main(){
 }
-
